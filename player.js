db374d77b3e03c7755015e715e6a295609e08a90
Steetan <vlad.no.96@bk.ru>
2023-05-14 10:25:30 +0300

    url fixed

--- player.js
+++ player.js
@@ -1,3 +1,6 @@
+window.location.href = "#" + localStorage.getItem('href') //чтобы в урл сохранялась переданная ссылка
+
 document.querySelector(".video").src 
     = localStorage.getItem('href')  //вытаскиваем урл из локального хранилища и вставляем в атрибут проигрывателя
+
 
