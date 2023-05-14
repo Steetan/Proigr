b445a68adaccc5ecc9b3559f623ff83fdcb8f928
Steetan <vlad.no.96@bk.ru>
2023-05-14 11:18:35 +0300

    url fixed 2

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let url = window.location.href //получаем урл сайта
+let url = url2 = window.location.href //получаем урл сайта
 
 if(window.location.href.includes("?=https://")) {  //проверяем вставили ли мы ссылку на видео
     url = window.location.href.toString().split("?=") //обрезаем урл
         .pop() //удаляем ненужный последний элемент
         .replace("watch?v=", "embed/") //делаем так чтобы проигрыватель работал
-
+    url2 = window.location.href.toString().split("?=").pop() //обрезаем урл
     window.localStorage.setItem('href', url)  //перекидываем полученный урл на локальное хранилище
+    window.localStorage.setItem('href2', url2)  //перекидываем полученный урл на локальное хранилище
     window.open("player.html", "_self")  //открываем проигрыватель
 }
-
--- player.js
+++ player.js
@@ -1,6 +1,4 @@
-window.location.href = "#" + localStorage.getItem('href') //чтобы в урл сохранялась переданная ссылка
+window.location.href = "#" + localStorage.getItem('href2') //чтобы в урл сохранялась переданная ссылка
 
 document.querySelector(".video").src 
     = localStorage.getItem('href')  //вытаскиваем урл из локального хранилища и вставляем в атрибут проигрывателя
-
-
