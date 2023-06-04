1572714488622465204596db95b5f9e36226027a
Благодар <62824780+6jlarogap@users.noreply.github.com>
2023-06-04 17:08:45 +0300

    Update player.js

--- player.js
+++ player.js
@@ -14,7 +14,6 @@ function getVotes() {
         type: 'GET',
         contentType: 'application/json; charset=utf-8',
         dataType: 'json',
-        }),
         success: function(data) {
             //  - поместить данные в таблицу
             console.log(data)
