ac073a8006dc3fad285d5d16fedfb46fbafaed78
Благодар <62824780+6jlarogap@users.noreply.github.com>
2023-06-12 08:28:59 +0300

    Update player.js

--- player.js
+++ player.js
@@ -30,8 +30,7 @@ function getVotes(auth_data) {
             for (let button in data.buttons) {
                 // проход по массиву из элементов {time: ..., count: ...}
                 for (let t of data.buttons[button]) {
-                    alert(typeof(t.time))
-                    if (timeGraphic.IndexOf(t.time) != -1) {
+                    if (timeGraphic.IndexOf(t.time.toString()) != -1) {
                         timeGraphic.push(t.time);
                     }
                 }
