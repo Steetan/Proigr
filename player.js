c3c48215a9a3aa960c0ec1e15e4d1f325a05aa36
Благодар <62824780+6jlarogap@users.noreply.github.com>
2023-06-17 10:25:28 +0300

    Update player.js

--- player.js
+++ player.js
@@ -77,7 +77,7 @@ var chart = new Chart(document.getElementById("graphic"), {
 
 async function delBtnEvent(event) {
     if(!auth_data || !event) return;
-    let vote_time = event.parentNode[]
+    let vote_time = event.parentNode
     var headers = auth_data ? { 'Authorization': 'Token ' + auth_data.auth_token } : {};
     try { 
         await $.ajax({
