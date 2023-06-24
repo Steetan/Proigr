2bd88b4061e1375c13dd49448c67fbbeac9f892b
Благодар <62824780+6jlarogap@users.noreply.github.com>
2023-06-24 07:02:48 +0300

    Update player.js

--- player.js
+++ player.js
@@ -291,7 +291,8 @@ async function getUserVotes() {
 
             document.querySelector("tbody").prepend(trTable) //засовываем в html созданную строку
             trTable.append(tdTable, td2Table, td3Table, td4Table)
-            document.querySelector(".delete-btn").onclick = function() { onDelBtnEvent(document.querySelector(".delete-btn")) } //ищем все кнопки удаления и ставим на них прослушку
+            td4Table.onclick = function() { onDelBtnEvent(this) } //ищем все кнопки удаления и ставим на них прослушку
+//            document.querySelector(".delete-btn").onclick = function() { onDelBtnEvent(document.querySelector(".delete-btn")) } //ищем все кнопки удаления и ставим на них прослушку
         }
         
     } else {
