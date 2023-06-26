5c9d1cc9548b484def0860a8833b4a14f09efdc0
Steetan <vlad.no.96@bk.ru>
2023-06-26 15:51:54 +0300

    proj fix upd 30

--- player.js
+++ player.js
@@ -58,6 +58,14 @@ var chart = new Chart(document.getElementById("graphic"), {
             title: {
                 display: true,
                 text: 'График нажатий кнопок по времени видео' //заголовок графика
+            },
+            zoom: {
+                pan: {
+                   enabled: true
+                },
+                zoom: {
+                   enabled: true
+                }
             }
         },
         scales: {
